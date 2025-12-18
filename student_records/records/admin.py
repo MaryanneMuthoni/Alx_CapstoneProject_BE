4a3126d3ff1ca186548014fdcc28516ac11aae1e
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-18 06:42:24 -0800

    Initial setting up of admin.py, urls.py and adjusting models.py

--- student_records/records/admin.py
+++ student_records/records/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Grade, Invoice, Parent, Subject, Teacher, Payment, Enrollment, Attendance, StudentParent, Performance, Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'gender', 'date_of_birth',  'status')
 
 # Register your models here.
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Parent)
+admin.site.register(StudentParent)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(Performance)
+admin.site.register(Attendance)
+admin.site.register(Invoice)
+admin.site.register(Payment)
+admin.site.register(Enrollment)
+admin.site.register(Grade)
