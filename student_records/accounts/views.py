2642f89963532e83d56f4313956824f5136ea65a
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-24 03:52:03 -0800

    User authentication
    Created accounts app specifically for authentication
    Created SignUpView, urls.py for login, logout, password change, password reset, profile view and templates

---
+++ student_records/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+# User Authentication views
+# =========================
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'records/signup.html'
