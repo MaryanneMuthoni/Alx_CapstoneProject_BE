a0ddda0403c0204af622fc35628a08dfcd4ffe2d
MaryanneMuthoni <muthoni.w.maryanne@gmail.com>
2025-12-24 21:05:27 +0300

    Add TemplateView for profile.html template to the accounts app views.py

--- student_records/accounts/views.py
+++ student_records/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 
@@ -12,3 +12,7 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'records/signup.html'
+
+class TemplateView(TemplateView):
+    '''Rendering the profile.html template'''
+    template_name = 'accounts/profile.html'
