d6736abcf84d66da07203f4643911e88e116a562
MaryanneMuthoni <muthoni.w.maryanne@gmail.com>
2026-01-02 22:03:17 -0800

    Initial viewset views in api app for DRF

--- student_records/api/views.py
+++ student_records/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import from .models import Student, Parent, Grade, Teacher, Performance, Attendance, Invoice, Payment, Enrollment, Subject
+from .serializers import StudeentSerializer, ParentSerializer, GradeSerializer, TeacherSerializer, PerformanceSerializer, AttendanceSerializer, InvoiceSerializer, PaymentSerializer, EnrollmentSerializer, SubjectSerializer
 
 # Create your views here.
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
