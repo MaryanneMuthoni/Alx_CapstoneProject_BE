a54203403d513f9ccf4aae2f6f67854b41c97f75
MaryanneMuthoni <muthoni.w.maryanne@gmail.com>
2026-01-03 09:23:49 +0300

    Update views.py: spelling error fix

--- student_records/api/views.py
+++ student_records/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import from .models import Student, Parent, Grade, Teacher, Performance, Attendance, Invoice, Payment, Enrollment, Subject
-from .serializers import StudeentSerializer, ParentSerializer, GradeSerializer, TeacherSerializer, PerformanceSerializer, AttendanceSerializer, InvoiceSerializer, PaymentSerializer, EnrollmentSerializer, SubjectSerializer
+from .serializers import StudentSerializer, ParentSerializer, GradeSerializer, TeacherSerializer, PerformanceSerializer, AttendanceSerializer, InvoiceSerializer, PaymentSerializer, EnrollmentSerializer, SubjectSerializer
 
 # Create your views here.
 
