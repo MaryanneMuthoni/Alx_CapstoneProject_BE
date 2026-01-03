f1b333af28dca659195309ac2cdb20b45427e481
MaryanneMuthoni <muthoni.w.maryanne@gmail.com>
2026-01-03 09:41:20 +0300

    Update urls.py: All Routers

--- student_records/api/urls.py
+++ student_records/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet, ParentViewSet, GradeViewSet, SubjectViewSet, TeacherViewSet, PerformanceViewSet, AttendanceViewSet, InvoiceViewSet, PaymentViewSet, PerformanceViewSet, EnrollmentViewSet
+from .views import StudentViewSet, ParentViewSet, GradeViewSet, SubjectViewSet, TeacherViewSet, AttendanceViewSet, InvoiceViewSet, PaymentViewSet, PerformanceViewSet, EnrollmentViewSet
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
+router.register(r'parents', ParentViewSet)
+router.register(r'grades', GradeViewSet)
+router.register(r'subjects', SubjectViewSet)
+router.register(r'teachers', TeacherViewSet)
+router.register(r'performances', PerformanceViewSet)
+router.register(r'attendances', AttendanceViewSet)
+router.register(r'invoices', InvoiceViewSet)
+router.register(r'payments', PaymentViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
