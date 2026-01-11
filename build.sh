d8179cf548752d0f7616c9531606080107fc525c
MaryanneMuthoni <muthoni.w.maryanne@gmail.com>
2026-01-11 00:37:08 -0800

    Preparing django project for deployment

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+
+# Apply migrations
+python3 student_records/manage.py migrate
+
+# Collect static files
+python3 student_records/manage.py collectstatic --noinput
 
--- student_records/student_records/settings.py
+++ student_records/student_records/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vqhsow*t8tfnloap%9ok%t89oac)^o-d5_5^^3-wll5mbnybfz'
+# SECRET_KEY = 'django-insecure-vqhsow*t8tfnloap%9ok%t89oac)^o-d5_5^^3-wll5mbnybfz'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
